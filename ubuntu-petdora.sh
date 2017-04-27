054ef4f465701fc1b810e26f2fddcd50cb54930d
xpsteven <xp@fandora.co>
2017-04-27 10:48:16 +0800

    bug fix

--- ubuntu-petdora.sh
+++ ubuntu-petdora.sh
@@ -1,4 +1,4 @@
 cd ubuntu
 sudo docker pull xpsteven/ubuntu:14.04
-sudo docker build -t xpsteven/ubuntu:fdrshop6 -f fdrshop6.Dockerfile .
-sudo docker push xpsteven/ubuntu:fdrshop6
+sudo docker build -t xpsteven/ubuntu:petdora -f ubuntu-petdora.dockerfile .
+sudo docker push xpsteven/ubuntu:petdora
