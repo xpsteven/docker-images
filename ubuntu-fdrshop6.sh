a5163de204b8fdcc40376fd5320d2aaf1bbead4d
xpsteven <xp@fandora.co>
2017-04-27 10:57:31 +0800

    bug fix

--- ubuntu-fdrshop6.sh
+++ ubuntu-fdrshop6.sh
@@ -1,4 +1,4 @@
 cd ubuntu
 sudo docker pull xpsteven/ubuntu:14.04
-sudo docker build -t xpsteven/ubuntu:fdrshop6 -f ubuntu-fdrshop6 .
+sudo docker build -t xpsteven/ubuntu:fdrshop6 -f ubuntu-fdrshop6.dockerfile .
 sudo docker push xpsteven/ubuntu:fdrshop6
